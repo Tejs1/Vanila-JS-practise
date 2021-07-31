109bbb2eb94fec98e34baaeabee0b9fc5d134989
Tejs1 <67797070+Tejs1@users.noreply.github.com>
2021-08-01 05:09:37 +0530

    done finally

--- App.js
+++ App.js
@@ -2,15 +2,35 @@
 // var welcome = "this scrpit works " + username;
 
 // alert(welcome);
+
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
-// console.log(outputDiv);
-// outputDiv.innerText = "blaahh";
-
-// console.log(btnTranslate);
-
 var textInput = document.querySelector("#text-input");
-function clickHandler() {
-  outputDiv.innerText = "gg " + textInput.value;
+
+//var url = "https://lessonfourapi.tejs1.repl.co/translate/yoda.json";
+var url = "https://api.funtranslations.com/translate/minion.json";
+function urlcreator(text) {
+  return url + "?text=" + text;
 }
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("server down, try after some time");
+}
+
 btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  var inputText = textInput.value;
+
+  fetch(urlcreator(inputText))
+    .then((response) => response.json())
+    .then(
+      (json) => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+      }
+      // console.log(json.contents.text)
+      // console.log(json.contents.translation)
+    )
+    .catch(errorHandler);
+}
